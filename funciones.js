27396de9d06d355cf74d60a8503fc26bc0cad0ad
Elias Gonzalez Barrena <eliasgonzalezbarrena@gmail.com>
2023-03-08 10:32:37 -0300

    Primera Escritura en el Repositorio

---
+++ funciones.js
@@ -0,0 +1,8 @@
+document.getElementById('YPP1').onclick = function () {
+    console.log('holo mundi')
+    window.open("https://drive.google.com/file/d/1eUFeeR7p3OvcpAH9gU0cVuKwVWpU4xzZ/view?usp=sharing")
+}
+document.getElementById('ITPT1').onclick = function () {
+    console.log('holo mundi')
+    window.open("https://drive.google.com/file/d/1ebXbxCHHYHInWSOFZvSa9Gq3UniRnMd1/view?usp=sharing")
+}
